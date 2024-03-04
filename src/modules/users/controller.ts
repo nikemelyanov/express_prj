35b32cd81bca287ec24d6c11a94ae928def024a0
nikemelyanov <emelyanovtime@gmail.com>
2024-03-04 14:07:19 +0300

    add new crud methods in user, add new user routes

--- src/modules/users/controller.ts
+++ src/modules/users/controller.ts
@@ -2,17 +2,42 @@ import { Request, Response } from "express";
 import { UsersService } from "./services";
 
 export class UsersController {
-    static async getUsers(req: Request, res: Response) {
-        const users = await UsersService.getUsers()
-        res.json(users) ;
-    }
-
     static async createUser(req: Request, res: Response) {
         const user = {
             name: req.body.name,
-            email: req.body.email
+            email: req.body.email,
         };
         const newUser = await UsersService.createUser(user);
         res.json(newUser);
+    }
+
+    static async getUsers(req: Request, res: Response) {
+        const users = await UsersService.getUsers();
+        res.json(users);
+    }
+
+    static async getUser(req: Request, res: Response) {
+        const userid = Number(req.params["userId"]);
+        const user = await UsersService.getUser(userid);
+        if (!user) return res.status(404).send("user not found");
+        res.json(user);
+    }
+
+    static async updateUser(req: Request, res: Response) {
+        const userid = Number(req.params["userId"]);
+        const user = {
+            name: req.body.name,
+            email: req.body.email,
+        };
+        const updatedUser = await UsersService.updateUser(userid, user);
+        if (!updatedUser) return res.status(404).send("user not found");
+        res.json(updatedUser);
+    }
+
+    static async deleteUser(req: Request, res: Response) {
+        const userid = Number(req.params["userId"]);
+        const deletedUser = await UsersService.deleteUser(userid);
+        if (!deletedUser) return res.status(404).send("user not found");
+        res.json(deletedUser);
     }
 }
--- src/modules/users/services.ts
+++ src/modules/users/services.ts
@@ -1,12 +1,24 @@
 import prisma from "../../prisma";
-import { CreateUserDto } from "./dto";
+import { CreateUserDto, UpdateUserDto } from "./dto";
 
 export class UsersService {
+    static async createUser(user: CreateUserDto) {
+        return await prisma.user.create({ data: user });
+    }
+
     static async getUsers() {
         return await prisma.user.findMany();
     }
 
-    static async createUser(user: CreateUserDto) {
-        return await prisma.user.create({ data: user });
+    static async getUser(userId: number) {
+        return await prisma.user.findUnique({ where: { id: userId } });
+    }
+
+    static async updateUser(userId: number, user: UpdateUserDto) {
+        return await prisma.user.update({ where: { id: userId }, data: user });
+    }
+
+    static async deleteUser(userId: number) {
+        return await prisma.user.delete({ where: { id: userId } });
     }
 }
